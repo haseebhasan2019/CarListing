acd1ac7eb8a6ea7b91709482c711aa1ce981f0b2
haseebhasan2019 <haseebhasan2019@gmail.com>
2021-01-12 20:11:34 -0500

    End Of Day commit

--- src/App.js
+++ src/App.js
@@ -15,17 +15,25 @@ function App() {
   function handleAddListing(e) {
     const name = make.current.value
     const name2 = model.current.value
-    if (name === '' || name2 === '') return 
+    const name3 = year.current.value
+    const name4 = price.current.value
+    const name5 = seller.current.value
+    if (name === '' || name2 === '' || name3 === '' || name4 === '' || name5 === '') return 
     setListings(prevListing => {
-      return [...prevListing, {id: uuidv4(), name: name + " " + name2, complete: false}]
+      return [...prevListing, {id: uuidv4(), name: "•   " + name + " " + name2 + " " + name3
+      + ": " + name4 + " -" + name5, complete: false}]
     })
     make.current.value = null
     model.current.value = null
+    year.current.value = null
+    price.current.value = null
+    seller.current.value = null
   }
   
   return (
     <>
-      <CarList cars = {cars}/>
+      <h1>Welcome to Haseeb's Automobile Listings!</h1>
+      <h2>Add a Listing:</h2>
       <input ref={make} type="text" placeholder="make"/>
       <input ref={model} type="text" placeholder="model"/>
       <input ref={year} type="text" placeholder="year"/>
@@ -33,6 +41,9 @@ function App() {
       <input ref={seller} type="text" placeholder="seller"/>
 
       <button onClick={handleAddListing}>Add Listing</button>
+      <h2>Our Listings:</h2>
+      <CarList cars = {cars}/>
+
     </>
   )
 }
